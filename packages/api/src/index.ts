5a2d2533b286f3fd1cdff9b28cc7bbd9b58d184e
EmmanuelM <meric.emmanuel@gmail.com>
2024-12-14 18:23:52 +0100

    Add host 0.0.0.0 to api in production

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -3,6 +3,6 @@ import { server } from "./server.ts";
 import './routes.ts'
 
 if (import.meta.env.PROD)
-  await server.listen({ port: 3000 });
+  await server.listen({ port: 3000, host: "0.0.0.0" });
 
 export { server };
